054cff8ef1c30fd84dedd3f805834c8146925b5b
Kristofer Rye <kristofer.rye@gmail.com>
2017-01-29 08:55:51 -0600

    Start throwing vitals into rye-hub

--- rye-hub.el
+++ rye-hub.el
@@ -1,3 +1,11 @@
-(message "wwwwwwwwwwwwwwwwwwwwwwwwwww")
+(message "Loading @rye's hub...")
+
+;; If we are on a Darwin (i.e. Mac) operating system,
+;; assume that we have a funky keyboard and make Option
+;; map to super and Command map to meta.
+(if (eq system-type 'darwin)
+    (progn
+      (setf mac-command-modifier 'meta)
+      (setf mac-option-modifier 'super)))
 
 (provide 'rye-hub)
