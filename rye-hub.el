a7df7880e7aee19481494e96ea3b7c681577a6fc
Kristofer Rye <kristofer.rye@gmail.com>
2017-02-05 11:57:52 -0600

    Delete darwin patches
    With kotct/dot@c2b4644bdd7d39ab89a86f4382cfb6186620936b, we no longer
    need this.

--- rye-hub.el
+++ rye-hub.el
@@ -1,38 +1,5 @@
 (kotct/personal-packages dockerfile-mode)
 
-(defun rye/system--get-term-program ()
-  (getenv "TERM_PROGRAM"))
-
-(defun rye/system--get-path ()
-  (getenv "PATH"))
-
-(defun rye/darwin-supplement ()
-  "Resets certain variables for Darwin (i.e. macOS) platforms."
-  (interactive "*")
-  (progn
-    ;; Map Option to Super and Command to Meta.
-    ;;
-    ;; This will make it easier for users coming from Linux systems to
-    ;; use the keyboard layout, and makes everything a bit more
-    ;; ergonomic.
-    (setf mac-command-modifier 'meta)
-    (setf mac-option-modifier 'super)
-
-    ;; If TERM_PROGRAM is not set, (i.e. during the load sequence) run
-    ;; the terminal to evaluate ~/.profile, ~/.zshrc, ~/.bashrc,
-    ;; etc. to build out the PATH variable.
-    ;;
-    ;; Then set `exec-path' to the split version of this PATH variable.
-    (if (not (rye/system--get-term-program))
-        (let ((path (shell-command-to-string
-                     "$SHELL -cl \"printf %s \\\"\\\$PATH\\\"\"")))
-          (setenv "PATH" path)
-          (setf exec-path (split-string (rye/system--get-path) ":"))))))
-
-;; Run `rye/darwin-supplement' if we are on Darwin systems.
-(if (eq system-type 'darwin)
-    (rye/darwin-supplement))
-
 ;; Set our font to Source Code Pro, size 12
 (set-face-attribute 'default nil :font "Source Code Pro-12")
 
