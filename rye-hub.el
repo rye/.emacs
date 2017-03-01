53f9c19797bca3e5974b6286b8646cd70e1a51a5
Kristofer Rye <kristofer.rye@gmail.com>
2017-03-01 09:38:28 -0600

    Add more functionality, font height setting key

--- rye-hub.el
+++ rye-hub.el
@@ -1,7 +1,20 @@
 (kotct/personal-packages dockerfile-mode)
 
 ;; Set our font to Source Code Pro, size 12
-(set-face-attribute 'default nil :font "Source Code Pro-12")
+(set-face-attribute 'default nil :font "Source Code Pro")
+
+;; Helper function to set the font height.
+(defun rye/set-font-height (height)
+  "Sets the `:height' face attribute on the `default' face to 10 times HEIGHT,
+which is standard for setting the font height."
+  (interactive "nNew font height: ")
+  (set-face-attribute 'default nil :height (floor (* 10 height))))
+
+;; Set the font height to 12.0 using our helper function.
+(rye/set-font-height 12.0)
+
+;; Keybind for font height setting.
+(global-set-key (kbd "C-x C-y") #'rye/set-font-height)
 
 ;; Use two-space tabs.
 (kotct/set-tab-width 2)
