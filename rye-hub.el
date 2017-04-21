230c01d58ccad12c63d0c241e0f8bf3230de6401
Kristofer Rye <kristofer.rye@gmail.com>
2017-04-21 11:16:44 -0500

    Remove old font scaling stuff
    Since this is in the upstream config now, we don't need this anymore.

--- rye-hub.el
+++ rye-hub.el
@@ -6,32 +6,6 @@
 ;; Keybind for font height setting.
 (global-set-key (kbd "C-x C-y") #'kotct/font-set-height)
 
-(defvar rye/font-scale-change-factor
-  1.0
-  "The proportion that the font height delta is multiplied by.")
-
-(defun rye/rescale-font-height (amount)
-  "Sets the `:height' face attribute on the `default' face to
-`rye/font-scale-change-factor' * `amount' + the current height."
-  (let ((new-size (+ (* rye/font-scale-change-factor amount) (/ (face-attribute 'default :height) 10.0))))
-    (set-face-attribute 'default nil :height (floor (* 10.0 new-size)))))
-
-(defun rye/upscale-font-height () (interactive "*") (rye/rescale-font-height +1.0))
-(defun rye/downscale-font-height () (interactive "*") (rye/rescale-font-height -1.0))
-(defun rye/reset-font-height () (interactive "*") (rye/set-font-height rye/default-font-height))
-
-
-;; Unbind stupid text scaling keybinds.
-(global-unset-key (kbd "C-x C-+"))
-(global-unset-key (kbd "C-x C-="))
-(global-unset-key (kbd "C-x C--"))
-(global-unset-key (kbd "C-x C-0"))
-
-(global-set-key (kbd "C-x C-+") #'rye/upscale-font-height)
-(global-set-key (kbd "C-x C-=") #'rye/upscale-font-height)
-(global-set-key (kbd "C-x C--") #'rye/downscale-font-height)
-(global-set-key (kbd "C-x C-0") #'rye/reset-font-height)
-
 ;; Use two-space tabs.
 (kotct/set-tab-width 2)
 
