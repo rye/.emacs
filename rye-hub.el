af482c7becf5d99d3b6e19e9d359e428e928f69f
Kristofer Rye <kristofer.rye@gmail.com>
2017-06-07 12:49:05 -0500

    Add two comments

--- rye-hub.el
+++ rye-hub.el
@@ -2,9 +2,11 @@
 
 (kotct/personal-packages dockerfile-mode gitignore-mode)
 
+;; Use Source Code Pro font, size 12.0.
 (kotct/font-set-name "Source Code Pro")
 (kotct/font-set-height 12.0)
 
+;; Ditto for new frames.
 (add-hook 'after-make-frame-functions (lambda (frame) (kotct/font-set-name "Source Code Pro")))
 (add-hook 'after-make-frame-functions (lambda (frame) (kotct/font-set-height 12.0)))
 
